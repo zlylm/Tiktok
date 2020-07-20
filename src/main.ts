6dfcf75ffce0fe91ed31a08de36bcdad0644996b
zlylm <912924984@qq.com>
2020-07-20 15:45:28 +0800

    初始化项目环境

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/less/reset.less'
+import '@/assets/less/border.less'
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+<style lang="less" scoped>
+.home{
+  font-size: .15rem;
+}
+</style>
